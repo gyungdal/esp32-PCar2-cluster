f76500ac5b424b43e0816f08a4c142671ba5ebab
Gyeongsik Kim <admin@gyungdal.cc>
2020-04-18 06:39:29 +0900

    ESP에 맞게 코드 수정

--- src/main.cpp
+++ src/main.cpp
@@ -9,94 +9,24 @@ AsyncUDP udp;
 const char* ssid = "***********";
 const char* password = "***********";
 
+unsigned int updateIndex(0);
+unsigned int indexChange(0);
+sParticipantsData pData;
+sParticipantsData pData2;
+sParticipantVehicleNamesData pVehicles;
+sParticipantVehicleNamesData pVehicles2;
+sVehicleClassNamesData pClasses;
+sGameStateData stateData;
+PacketBase packetHeader;
+
+bool participantsReceived(false);
+bool participantsReceived2(false);
+bool stateReceived(false);
+bool vehiclesReceived(false);
+bool vehiclesReceived2(false);
+
 void setup() {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    ESP_LOGE("WiFi", "Connect Failed");
-    while (1) {
-      delay(1000);
-    }
-  }
-  if (udp.listen(1234)) {
-    Serial.print("UDP Listening on IP: ");
-    Serial.println(WiFi.localIP());
-    udp.onPacket([](AsyncUDPPacket packet) {
-      Serial.print("UDP Packet Type: ");
-      Serial.print(packet.isBroadcast()
-                       ? "Broadcast"
-                       : packet.isMulticast() ? "Multicast" : "Unicast");
-      Serial.print(", From: ");
-      Serial.print(packet.remoteIP());
-      Serial.print(":");
-      Serial.print(packet.remotePort());
-      Serial.print(", To: ");
-      Serial.print(packet.localIP());
-      Serial.print(":");
-      Serial.print(packet.localPort());
-      Serial.print(", Length: ");
-      Serial.print(packet.length());
-      Serial.print(", Data: ");
-      Serial.write(packet.data(), packet.length());
-      Serial.println();
-      // reply to the client
-      packet.printf("Got %u bytes of data", packet.length());
-    });
-  }
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-int main() {
-  unsigned short port(SMS_UDP_PORT);
-  char packetBuffer[SMS_UDP_MAX_PACKETSIZE];
-  PacketBase packetHeader;
   memset(&packetHeader, 0, sizeof(PacketBase));
-
-  int iResult(0);
-  WSADATA wsaData;
-  SOCKET RecvSocket;
-
-  sockaddr_in Receiver;
-  Receiver.sin_family = AF_INET;
-  Receiver.sin_port = htons(port);
-  Receiver.sin_addr.s_addr = htonl(INADDR_ANY);
-
-  sockaddr_in Sender;
-  int SenderAddrSize(sizeof(Sender));
-
-  iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-  if (iResult != NO_ERROR) {
-    wprintf(L"WSAStartup failed with error %d\n", iResult);
-    return 1;
-  }
-
-  RecvSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if (RecvSocket == INVALID_SOCKET) {
-    wprintf(L"socket failed with error %d\n", WSAGetLastError());
-    return 1;
-  }
-
-  iResult = bind(RecvSocket, (SOCKADDR*)&Receiver, sizeof(Receiver));
-  if (iResult != 0) {
-    wprintf(L" Failed to bind to socket with error %d\n", WSAGetLastError());
-    return 1;
-  }
-
-  //------------------------------------------------------------------------------
-  // TEST DISPLAY CODE
-  //------------------------------------------------------------------------------
-  unsigned int updateIndex(0);
-  unsigned int indexChange(0);
-  sParticipantsData pData;
-  sParticipantsData pData2;
-  sParticipantVehicleNamesData pVehicles;
-  sParticipantVehicleNamesData pVehicles2;
-  sVehicleClassNamesData pClasses;
-  sGameStateData stateData;
-
   memset(&pData, 0, sizeof(sParticipantsData));
   memset(&pData2, 0, sizeof(sParticipantsData));
   memset(&stateData, 0, sizeof(sGameStateData));
@@ -104,130 +34,126 @@ int main() {
   memset(&pVehicles2, 0, sizeof(sParticipantVehicleNamesData));
   memset(&pClasses, 0, sizeof(sVehicleClassNamesData));
 
-  bool participantsReceived(false);
-  bool participantsReceived2(false);
-  bool stateReceived(false);
-  bool vehiclesReceived(false);
-  bool vehiclesReceived2(false);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    ESP_LOGE("WiFi", "Connect Failed");
+    ESP.restart();
+  }
+  if (udp.listen(SMS_UDP_PORT)) {
+    ESP_LOGI("UDP", "UDP Listening on IP: %s", WiFi.localIP().toString().c_str());
+    udp.onPacket([](AsyncUDPPacket packet) {
+      ESP_LOGD("UDP", "UDP Packet Type: %s", packet.isBroadcast()
+                       ? "Broadcast"
+                       : packet.isMulticast() ? "Multicast" : "Unicast");
+      ESP_LOGD("UDP", "From: %s:%u", packet.remoteIP().toString().c_str(),packet.remotePort());
+      ESP_LOGD("UDP", "To : %s:%u", packet.localIP().toString().c_str(), packet.localPort());
+      ESP_LOGD("UDP", "Len : %d", static_cast<int32_t>(packet.length()));
+      parsePacket(packet.data(), packet.length());
+    });
+  }
+}
 
-  printf("ESC TO EXIT\n\n");
-  while (true) {
-    printf("Receiving Packets \n");
-    iResult = recvfrom(RecvSocket, packetBuffer, SMS_UDP_MAX_PACKETSIZE, 0,
-                       (SOCKADDR*)&Sender, &SenderAddrSize);
-    if (iResult == SOCKET_ERROR) {
-      wprintf(L"recvfrom failed with error %d\n", WSAGetLastError());
-    } else {
-      if (iResult > 0) {
-        memcpy(&packetHeader, packetBuffer, sizeof(PacketBase));
-        switch (packetHeader.mPacketType) {
-          case eParticipants:
-            if (packetHeader.mPartialPacketIndex == 1) {
-              memcpy(&pData, packetBuffer, sizeof(sParticipantsData));
-            }
-            if (packetHeader.mPartialPacketIndex == 2) {
-              memcpy(&pData2, packetBuffer, sizeof(sParticipantsData));
-              participantsReceived2 = true;
-            }
-            if (packetHeader.mPartialPacketIndex ==
-                packetHeader.mPartialPacketNumber) {
-              participantsReceived = true;
-            }
-
-            break;
-          case eGameState:
-            memcpy(&stateData, packetBuffer, sizeof(sGameStateData));
-            stateReceived = true;
-            break;
-          case eParticipantVehicleNames: {
-            // last packet are always the vehicle class names
-            if (packetHeader.mPartialPacketIndex ==
-                packetHeader.mPartialPacketNumber) {
-              memcpy(&pClasses, packetBuffer, sizeof(sVehicleClassNamesData));
-              vehiclesReceived = true;
-            } else {
-              if (packetHeader.mPartialPacketIndex == 1) {
-                memcpy(&pVehicles, packetBuffer,
-                       sizeof(sParticipantVehicleNamesData));
-              }
-              if (packetHeader.mPartialPacketIndex == 2) {
-                memcpy(&pVehicles2, packetBuffer,
-                       sizeof(sParticipantVehicleNamesData));
-              }
-            }
-          }
-          default:
-            break;
+void parsePacket(uint8_t* packetBuffer, size_t length) {
+  if (length > 0) {
+    memcpy(&packetHeader, packetBuffer, sizeof(PacketBase));
+    switch (packetHeader.mPacketType) {
+      case eParticipants:
+        if (packetHeader.mPartialPacketIndex == 1) {
+          memcpy(&pData, packetBuffer, sizeof(sParticipantsData));
         }
-      }
-    }
-    wprintf(
-        L" Last packet size %d, Packet count %d, packet type count %d, group "
-        L"index %d, total in group %d - of type \n",
-        iResult, packetHeader.mPacketNumber, packetHeader.mCategoryPacketNumber,
-        packetHeader.mPartialPacketIndex, packetHeader.mPartialPacketNumber,
-        packetHeader.mPacketVersion);
-    if (stateReceived) {
-      int gameState = stateData.mGameState & 7;
-      int sessionState = stateData.mGameState >> 4;
-
-      wprintf(L" Game State %i, gameState  %i, sessionState %i \n",
-              stateData.mGameState, gameState, sessionState);
-      wprintf(L" Race Participants  \n");
-      if (participantsReceived) {
-        for (int i(0); i < PARTICIPANTS_PER_PACKET; ++i) {
-          if (pData.sName[i][0] != '\0') {
-            wprintf(L" Name %S \n", pData.sName[i]);
-          }
+        if (packetHeader.mPartialPacketIndex == 2) {
+          memcpy(&pData2, packetBuffer, sizeof(sParticipantsData));
+          participantsReceived2 = true;
         }
-        if (participantsReceived2) {
-          for (int i(0); i < PARTICIPANTS_PER_PACKET; ++i) {
-            if (pData2.sName[i][0] != '\0') {
-              wprintf(L" Name %S \n", pData2.sName[i]);
-            }
+        if (packetHeader.mPartialPacketIndex ==
+            packetHeader.mPartialPacketNumber) {
+          participantsReceived = true;
+        }
+
+        break;
+      case eGameState:
+        memcpy(&stateData, packetBuffer, sizeof(sGameStateData));
+        stateReceived = true;
+        break;
+      case eParticipantVehicleNames: {
+        // last packet are always the vehicle class names
+        if (packetHeader.mPartialPacketIndex ==
+            packetHeader.mPartialPacketNumber) {
+          memcpy(&pClasses, packetBuffer, sizeof(sVehicleClassNamesData));
+          vehiclesReceived = true;
+        } else {
+          if (packetHeader.mPartialPacketIndex == 1) {
+            memcpy(&pVehicles, packetBuffer,
+                   sizeof(sParticipantVehicleNamesData));
+          }
+          if (packetHeader.mPartialPacketIndex == 2) {
+            memcpy(&pVehicles2, packetBuffer,
+                   sizeof(sParticipantVehicleNamesData));
           }
         }
       }
-      if (vehiclesReceived) {
-        wprintf(L"Vehicle Names\n");
-        for (int i(0); i < VEHICLES_PER_PACKET; ++i) {
-          if (pVehicles.sVehicles[i].sName[0] != '\0') {
-            wprintf(L"Vehicle Name %S, index %d, class %d \n",
-                    pVehicles.sVehicles[i].sName[i],
-                    pVehicles.sVehicles[i].sIndex,
-                    pVehicles.sVehicles[i].sClass);
-          }
-        }
-        if (vehiclesReceived2) {
-          for (int i(0); i < VEHICLES_PER_PACKET; ++i) {
-            if (pVehicles2.sVehicles[i].sName[0] != '\0') {
-              wprintf(L"Vehicle Name %S, index %d, class %d \n",
-                      pVehicles2.sVehicles[i].sName[i],
-                      pVehicles2.sVehicles[i].sIndex,
-                      pVehicles2.sVehicles[i].sClass);
-            }
-          }
-        }
-        wprintf(L"Class Names\n");
-        for (int i(0); i < CLASSES_SUPPORTED_PER_PACKET; ++i) {
-          if (pClasses.sClasses[i].sName[0] != '\0') {
-            wprintf(L"Class Name %S, index %d`\n", pClasses.sClasses[i].sName,
-                    pClasses.sClasses[i].sClassIndex);
-          }
-        }
-      }
-    }
-
-    system("cls");
-
-    if (_kbhit() && _getch() == 27)  // check for escape
-    {
-      break;
+      default:
+        break;
     }
   }
-  //------------------------------------------------------------------------------
 
-  closesocket(RecvSocket);
+  ESP_LOGI("Log",
+      " Last packet size %d, Packet count %d, packet type count %d, group index %d, total in group %d - of type \n",
+      length, packetHeader.mPacketNumber, packetHeader.mCategoryPacketNumber,
+      packetHeader.mPartialPacketIndex, packetHeader.mPartialPacketNumber,
+      packetHeader.mPacketVersion);
+  if (stateReceived) {
+    int gameState = stateData.mGameState & 7;
+    int sessionState = stateData.mGameState >> 4;
 
-  return 0;
+    wprintf(L" Game State %i, gameState  %i, sessionState %i \n",
+            stateData.mGameState, gameState, sessionState);
+    wprintf(L" Race Participants  \n");
+    if (participantsReceived) {
+      for (int i(0); i < PARTICIPANTS_PER_PACKET; ++i) {
+        if (pData.sName[i][0] != '\0') {
+          wprintf(L" Name %S \n", pData.sName[i]);
+        }
+      }
+      if (participantsReceived2) {
+        for (int i(0); i < PARTICIPANTS_PER_PACKET; ++i) {
+          if (pData2.sName[i][0] != '\0') {
+            wprintf(L" Name %S \n", pData2.sName[i]);
+          }
+        }
+      }
+    }
+    if (vehiclesReceived) {
+      wprintf(L"Vehicle Names\n");
+      for (int i(0); i < VEHICLES_PER_PACKET; ++i) {
+        if (pVehicles.sVehicles[i].sName[0] != '\0') {
+          wprintf(L"Vehicle Name %S, index %d, class %d \n",
+                  pVehicles.sVehicles[i].sName[i],
+                  pVehicles.sVehicles[i].sIndex, pVehicles.sVehicles[i].sClass);
+        }
+      }
+      if (vehiclesReceived2) {
+        for (int i(0); i < VEHICLES_PER_PACKET; ++i) {
+          if (pVehicles2.sVehicles[i].sName[0] != '\0') {
+            wprintf(L"Vehicle Name %S, index %d, class %d \n",
+                    pVehicles2.sVehicles[i].sName[i],
+                    pVehicles2.sVehicles[i].sIndex,
+                    pVehicles2.sVehicles[i].sClass);
+          }
+        }
+      }
+      wprintf(L"Class Names\n");
+      for (int i(0); i < CLASSES_SUPPORTED_PER_PACKET; ++i) {
+        if (pClasses.sClasses[i].sName[0] != '\0') {
+          wprintf(L"Class Name %S, index %d`\n", pClasses.sClasses[i].sName,
+                  pClasses.sClasses[i].sClassIndex);
+        }
+      }
+    }
+  }
+}
+void loop() { 
+  delay(1000); 
 }
